30eb2bf746cf8185834ff355f2da9504936eb0c3
RatiuC <ratiu.cristi98@gmail.com>
2019-08-13 17:13:11 +0300

    After Working

--- PieseAuto/hello.py
+++ PieseAuto/hello.py
@@ -1,6 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def problema(x):
+    return ((4+3*x)*2)+((3-4*i)*2)
+i=-1
+print("{} cu valoarea -1 problema este egala cu {}".format(i,problema(i)))
